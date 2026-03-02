ef5b3d7846b8e23b9076bbc2cffcb9fe15f09a3a
Byeolunu <mouftahnouhaila@gmail.com>
2026-03-02 13:26:20 +0000

    .

--- app/src/main/java/com/example/app/GridRecyclerActivity.java
+++ app/src/main/java/com/example/app/GridRecyclerActivity.java
@@ -1,4 +1,86 @@
 package com.example.app;
 
-public class GridRecyclerActivity {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GridRecyclerActivity extends AppCompatActivity {
+    String[] countries = {
+            "Maroc", "France", "USA", "Japon", "Canada",
+            "Allemagne", "Italie", "Brésil", "Argentine", "Égypte",
+            "Afrique du Sud", "Chine", "Inde", "Russie", "Mexique",
+            "Nigéria", "Corée du Sud"
+    };
+
+    int[] flags = {
+            R.drawable.maroc, R.drawable.france, R.drawable.usa, R.drawable.japon, R.drawable.canada,
+            R.drawable.allemagne, R.drawable.italy, R.drawable.brezil, R.drawable.argentina, R.drawable.egypte,
+            R.drawable.afs, R.drawable.china, R.drawable.india, R.drawable.russia, R.drawable.meique,
+            R.drawable.nigeria, R.drawable.south_korea
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid_recycler);
+
+        RecyclerView recyclerView = findViewById(R.id.countryGrid);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        CountryRecyclerAdapter adapter = new CountryRecyclerAdapter(countries, flags);
+        recyclerView.setAdapter(adapter);
+    }
+
+    class CountryRecyclerAdapter extends RecyclerView.Adapter<CountryRecyclerAdapter.ViewHolder> {
+
+        private String[] countryNames;
+        private int[] countryFlags;
+
+        public CountryRecyclerAdapter(String[] names, int[] flags) {
+            this.countryNames = names;
+            this.countryFlags = flags;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item_country, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.tvName.setText(countryNames[position]);
+            holder.ivFlag.setImageResource(countryFlags[position]);
+
+            holder.itemView.setOnClickListener(v ->
+                    Toast.makeText(GridRecyclerActivity.this, countryNames[position], Toast.LENGTH_SHORT).show()
+            );
+        }
+
+        @Override
+        public int getItemCount() {
+            return countryNames.length;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView ivFlag;
+            TextView tvName;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                ivFlag = itemView.findViewById(R.id.gridImage);
+                tvName = itemView.findViewById(R.id.gridText);
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/app/ListActivity.java
+++ app/src/main/java/com/example/app/ListActivity.java
@@ -1,6 +1,14 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +18,72 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ListActivity extends AppCompatActivity {
 
+    String[] countries = {
+            "Maroc", "France", "USA", "Japon", "Canada",
+            "Allemagne", "Italie", "Brésil", "Argentine", "Égypte",
+            "Afrique du Sud", "Chine", "Inde", "Russie", "Mexique",
+            "Nigéria", "Corée du Sud"
+    };
+
+    int[] flags = {
+            R.drawable.maroc, R.drawable.france, R.drawable.usa, R.drawable.japon, R.drawable.canada,
+            R.drawable.allemagne, R.drawable.italy, R.drawable.brezil, R.drawable.argentina, R.drawable.egypte,
+            R.drawable.afs, R.drawable.china, R.drawable.india, R.drawable.russia, R.drawable.meique,
+            R.drawable.nigeria, R.drawable.south_korea
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_list);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ListView listView = findViewById(R.id.countryListView);
+
+        CountryAdapter adapter = new CountryAdapter();
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedCountry = countries[position];
+            Toast.makeText(ListActivity.this, "Pays sélectionné : " + selectedCountry, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    class CountryAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return countries.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return countries[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(ListActivity.this).inflate(R.layout.list_item_country, parent, false);
+            }
+
+            ImageView flagView = convertView.findViewById(R.id.countryFlag);
+            TextView nameView = convertView.findViewById(R.id.countryName);
+
+            flagView.setImageResource(flags[position]);
+            nameView.setText(countries[position]);
+
+            return convertView;
+        }
     }
 }
